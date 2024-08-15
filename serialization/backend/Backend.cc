860fd5f60e2c14735dbb42dcf33f0913498f7d8d
roopereku <roopereku@gmail.com>
2024-08-15 18:41:32 +0300

    Initial work on serialization backend

---
+++ serialization/backend/Backend.cc
@@ -0,0 +1,5 @@
+#include <autoglue/serialization/Backend.hh>
+
+namespace ag::serialization
+{
+}
---
+++ serialization/backend/include/autoglue/serialization/Backend.hh
@@ -0,0 +1,13 @@
+#ifndef AUTOGLUE_SERIALIZATION_BACKEND_HH
+#define AUTOGLUE_SERIALIZATION_BACKEND_HH
+
+#include <autoglue/Backend.hh>
+
+namespace ag::serialization
+{
+
+class Backend : public ag::Backend
+{
+};
+
+}
